afad5da9ddc07d77ce3e9f9cc1e0489013479290
chukurushka <118333803+chukurushka@users.noreply.github.com>
2023-03-04 14:47:30 +0700

    Add files via upload

---
+++ Practice 2.2.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using _2;
+
+namespace _1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student pat = new Student();
+
+            int res = 0;
+            Console.WriteLine("Меню выбора:\n" +
+                "1-изменить ФИО\n" +
+                "2-изменить дату рождения\n" +
+                "3-изменить группу\n" +
+                "4-вывод информации\n" +
+                "5-выход из меню\n");
+
+            while (res != 5)
+            {
+                res = Convert.ToInt32(Console.ReadLine());
+                if (res == 5) break;
+
+                switch (res)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите ФИО");
+                            pat.FullName = Console.ReadLine();
+                            Console.WriteLine($"новое ФИО: {pat.FullName}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Введите дату");
+                            pat.Date = Console.ReadLine();
+                            Console.WriteLine($"новая дата: {pat.Date}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите группу");
+                            pat.Group = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine($"новая группа: {pat.Group}");
+                            break;
+                        }
+                    case 4:
+                        Console.WriteLine($"ФИО: {pat.FullName}");
+                        Console.WriteLine($"Дата рождения: {pat.Date}");
+                        Console.WriteLine($"Группа: {pat.Group}");
+                        Console.Write($"Успеваемость: ");
+                        foreach (var num in pat.Performance)
+                        {
+                            Console.Write($"{num} ");
+                        }
+                        Console.WriteLine(" ");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Practice 2.2.1/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace _2
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public string Date { get; set; }
+        public int Group { get; set; }
+        public int[] Performance { get; set; }
+        public Student()
+        {
+            FullName = "Шумский Эдуард Игоревич";
+            Date = "02.02.2004";
+            Group = 623;
+            Performance = new int[5] { 2,2,3,2,2 };
+        }
+    }
+}
